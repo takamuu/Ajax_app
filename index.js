ef3b3259d84c0bdd069fce4b629e531ef63c826d
takashi <mr.takamuu@gmail.com>
2021-07-31 08:06:03 +0900

    Create DOM HTTP

--- index.js
+++ index.js
@@ -1,37 +1,68 @@
+/* eslint-disable no-use-before-define */
 /* eslint-disable no-shadow */
 /* eslint-disable no-undef */
 /* eslint-disable consistent-return */
 /* eslint-disable no-unused-vars */
 /* eslint-disable no-console */
-console.log('index.js: loaded');
-
-const userId = 'js-primer-example';
-function fetchUserInfo(userId) {
-  fetch(`https://api.github.com/users/${encodeURIComponent(userId)}`)
-    .then((response) => {
-      console.log(response.status);
-      // エラーレスポンスが返されたことを検知する
-      if (!response.ok) {
-        console.error('エラーレスポンス', response);
-      } else {
-        return response.json().then((userInfo) => {
-          // JSONパースされたオブジェクトが渡される
-          console.log(userInfo);
-        });
-      }
-    }).catch((error) => {
-      console.error(error);
+function main() {
+  fetchUserInfo('takamuu')
+    .catch((error) => {
+      // Promiseチェーンの中で発生したエラーを受け取る
+      console.error(`エラーが発生しました(${error})`);
     });
 }
 
-// var lines = [];
-// var reader = require('readline').createInterface({
-//   input: process.stdin,
-//   output: process.stdout
-// });
-// reader.on('line', (line) => {
-//   lines.push(line);
-// });
-// reader.on('close', () => {
-//   console.log(lines[0]);
-// });
+function fetchUserInfo(userId) {
+  // fetchの返り値のPromiseをreturnする
+  return fetch(`https://api.github.com/users/${encodeURIComponent(userId)}`)
+    .then((response) => {
+      // エラーレスポンスが返されたことを検知する
+      if (!response.ok) {
+        // エラーレスポンスからRejectedなPromiseを作成して返す
+        return Promise.reject(new Error(`${response.status}: ${response.statusText}`));
+      }
+      return response.json().then((userInfo) => {
+        // HTMLの組み立て
+        const view = createView(userInfo);
+        // HTMLの挿入
+        displayView(view);
+      });
+    });
+}
+
+function createView(userInfo) {
+  return escapeHTML`
+  <h4>${userInfo.name} (@${userInfo.login})</h4>
+  <img src="${userInfo.avatar_url}" alt="${userInfo.login}" height="100">
+  <dl>
+    <dt>Location</dt>
+    <dd>${userInfo.location}</dd>
+    <dt>Repositories</dt>
+    <dd>${userInfo.public_repos}</dd>
+  </dl>
+  `;
+}
+
+function displayView(view) {
+  const result = document.getElementById('result');
+  result.innerHTML = view;
+}
+
+function escapeSpecialChars(str) {
+  return str
+    .replace(/&/g, '&amp;')
+    .replace(/</g, '&lt;')
+    .replace(/>/g, '&gt;')
+    .replace(/"/g, '&quot;')
+    .replace(/'/g, '&#039;');
+}
+
+function escapeHTML(strings, ...values) {
+  return strings.reduce((result, str, i) => {
+    const value = values[i - 1];
+    if (typeof value === 'string') {
+      return result + escapeSpecialChars(value) + str;
+    }
+    return result + String(value) + str;
+  });
+}
